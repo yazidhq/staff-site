3cc62988fda5783610ad076165728cc269cc50bd
yazid dhiaulhaq ismail <103630151+yazidhq@users.noreply.github.com>
2022-12-02 20:24:46 +0700

    Update bot.php

--- template/bot.php
+++ template/bot.php
@@ -5,5 +5,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.js"></script>
 
+<script src="../assets/js/alert.js" type="text/javascript"></script>
+	
 </body>
 </html>
